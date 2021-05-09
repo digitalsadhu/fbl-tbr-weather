684966befbb7b096c169d9cbe2b99028625f0275
Richard <digitalsadhu@gmail.com>
2021-05-09 22:43:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import fastify from "fastify";
+import nunjucks from "nunjucks";
+import pov from "point-of-view";
+import roller from './roller.js';
+
+const app = fastify();
+
+app.register(pov, { engine: { nunjucks } });
+
+app.get("/weather", async (request, reply) => {
+  reply.view('templates/weather.njk');
+});
+
+app.get("/weather/result", (request, reply) => {
+    const result = roller();
+    reply.view('templates/result.njk', result);
+});
+
+app.listen(process.env.PORT || 3000, "0.0.0.0");
---
+++ roller.js
@@ -0,0 +1,48 @@
+import fs from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const wind = JSON.parse(
+  fs.readFileSync(join(__dirname, "./data/wind.json"), "utf-8")
+);
+const snowfall = JSON.parse(
+  fs.readFileSync(join(__dirname, "./data/snowfall.json"), "utf-8")
+);
+const cold = JSON.parse(
+  fs.readFileSync(join(__dirname, "./data/cold.json"), "utf-8")
+);
+
+const d6 = () => Math.ceil(Math.random() * 6);
+
+const rollWind = () => {
+  const roll = d6();
+  if (roll < 4) return wind[0];
+  if (roll < 6) return wind[1];
+  return wind[2];
+};
+
+const rollSnowfall = () => {
+  const roll = d6();
+  if (roll < 4) return snowfall[0];
+  if (roll < 6) return snowfall[1];
+  return snowfall[2];
+};
+
+const rollCold = (coldModifier) => {
+  const roll = d6() + coldModifier;
+  if (roll < 4) return cold[0];
+  if (roll < 6) return cold[1];
+  return cold[2];
+};
+
+const roller = () => {
+  const windRoll = rollWind();
+  const snowfallRoll = rollSnowfall();
+  const coldRoll = rollCold(windRoll.cold);
+
+  return { wind: windRoll, snow: snowfallRoll, cold: coldRoll };
+};
+
+export default roller;
